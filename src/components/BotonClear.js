def5e3f94bf2dbec4144a144c3f2128490ff377e
Fernando Perez <fernandrperez@gmail.com>
2023-03-23 18:03:16 -0300

    first commit

---
+++ src/components/BotonClear.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/BotonClear.css'
+const BotonClear = (props) => {
+  return (
+    <div className='boton-clear' onClick={props.manejarClear}>
+        {props.children}
+    </div>
+  )
+}
+
+export default BotonClear
